f550ec2f930ffc0323abf82c331a18e2766a074c
Kameron Johnson, Lee Jorgensen and Matt Terrill <leejorgensen22@gmail.com>
2015-03-10 11:04:54 -0700

    add brackets for each region

--- router.js
+++ router.js
@@ -2,5 +2,10 @@ MarchMadness.Router.map(function(){
   this.resource("home", {path: "/"});
   this.resource("brackets");
   this.resource("new-bracket");
-  this.resource("bracket", {path: "/:bracket_id"});
+  this.resource("bracket", {path: ":bracket_id"}, function() {
+    this.resource("region1");
+    this.resource("region2");
+    this.resource("region3");
+    this.resource("region4");
+  });
 });
