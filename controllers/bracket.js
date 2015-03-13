7d13f47b77ca4308b5759de9d45f3ea21efd04ae
Lee Jorgensen and Matt Terrill <meterrill@gmail.com>
2015-03-13 09:51:13 -0700

    add functionality to prevent teams from same game advancing

--- controllers/bracket.js
+++ controllers/bracket.js
@@ -1,11 +1,13 @@
 MarchMadness.BracketController = Ember.ObjectController.extend({
   actions:{
     advance:function(round, team, game){
+      team.previousGameId = game.id;
       var nextRound = round.roundNumber;
       var nextRoundGames = this.model._data.rounds[nextRound].games
       var nextGame = null;
+
       var indexOfNextGame = nextRoundGames.forEach(function(thisGame) {
-        if ((thisGame.id === game.nextGameId) && (thisGame.teams.length < 2)) {
+        if ((thisGame.id === game.nextGameId) && (thisGame.teams.length < 2) && (game.teams[0].previousGameId !== game.teams[1].previousGameId)) {
           nextGame = thisGame;
         }
       });
--- models/team.js
+++ models/team.js
@@ -2,5 +2,6 @@ MarchMadness.Team = DS.Model.extend({
   school: DS.attr(),
   region: DS.attr(),
   seed: DS.attr(),
+  previousGameId: DS.attr(),
   round: DS.belongsTo('round', {async: true})
 });
